7b3d3d05d043f7feaf8933d77a205f7c55f5305f
jpchadwick92 <jpchadwick92@gmail.com>
2022-09-05 20:56:33 +0100

    add basic error handling

--- Controllers/categories.controllers.js
+++ Controllers/categories.controllers.js
@@ -1,7 +1,9 @@
 const { fetchCategories } = require("../Models/categories.models");
 
-exports.getCategories = (req, res) => {
-  fetchCategories().then((categories) => {
-    res.status(200).send({ categories });
-  });
+exports.getCategories = (req, res, next) => {
+  fetchCategories()
+    .then((categories) => {
+      res.status(200).send({ categories });
+    })
+    .catch(next);
 };
--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const app = express();
 
 app.get("/api/categories", getCategories);
 
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send("Internal server error");
+});
+
 module.exports = app;
