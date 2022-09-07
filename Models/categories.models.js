b2299085b764d61e5833d88246df47e29b2e69e0
jpchadwick92 <jpchadwick92@gmail.com>
2022-09-07 10:57:12 +0100

    add comment_count to GET reviews response

--- Models/categories.models.js
+++ Models/categories.models.js
@@ -8,7 +8,16 @@ exports.fetchCategories = () => {
 
 exports.fetchReviewsById = (review_id) => {
   return db
-    .query("SELECT * FROM reviews WHERE review_id=$1", [review_id])
+    .query(
+      `
+      SELECT reviews.*, COUNT(comments.review_id)::INT AS comment_count
+      FROM reviews 
+      LEFT JOIN comments ON reviews.review_id = comments.review_id
+      WHERE reviews.review_id=$1
+      GROUP BY reviews.review_id
+ ;`,
+      [review_id]
+    )
     .then(({ rows }) => {
       if (!rows[0]) {
         return Promise.reject({ status: 404, msg: "review does not exist" });
