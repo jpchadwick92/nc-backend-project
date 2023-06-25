673f4c9d2d99252b68c8835740ab003b31aceff2
jpchadwick92 <jpchadwick92@gmail.com>
2023-06-25 23:49:58 +0100

    add happy path for PATCH /api/comments/:comment_id

--- Models/comments.models.js
+++ Models/comments.models.js
@@ -83,3 +83,17 @@ exports.removeComment = (comment_id) => {
       );
     });
 };
+
+exports.updateComment = (inc_votes, comment_id) => {
+  return db
+    .query(
+      `UPDATE comments 
+      SET votes = votes + $1 
+      WHERE comment_id=$2 
+      RETURNING *`,
+      [inc_votes, comment_id]
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -502,4 +502,25 @@ describe("/api/comments/:comment_id", () => {
         });
     });
   });
+  describe("PATCH", () => {
+    test("200: updates votes property and responds with updated comment", () => {
+      const incrementVotes = { inc_votes: -1 };
+      return request(app)
+        .patch("/api/comments/1")
+        .send(incrementVotes)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.comment).toEqual(
+            expect.objectContaining({
+              comment_id: 1,
+              body: "I loved this game too!",
+              votes: 15,
+              author: "bainesface",
+              review_id: 2,
+              created_at: "2017-11-22T12:43:33.389Z",
+            })
+          );
+        });
+    });
+  });
 });
